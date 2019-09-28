91f3dc5481239698e83fd76b24ce9b1f56b86c48
jayyu224.itl02g@nctu.edu.tw <qwert12345J>
2019-09-29 00:58:32 +0800

    delete unused comments

--- csvCloud.py
+++ csvCloud.py
@@ -4,16 +4,12 @@ from bs4 import BeautifulSoup
 import jieba
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
-# import sys
-# print(sys.executable)
 
 feedback = xlrd.open_workbook(r'yoursheet.xlsx') # use the file name of your sheet
 #print(feedback.sheet_names())
 
 content1 = feedback.sheet_by_index(0)
-
 result = content1.col_values(0)
-#print(type(result))
 
 allText = ""
 
